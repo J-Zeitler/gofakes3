60bfae3843fd983dbb203dfc96badba38edb7359
Jonas Zetiler <jonas.zeitler@modularfinance.se>
2026-03-31 12:02:08 +0200

    treat empty delimiter as no delimiter

--- prefix.go
+++ prefix.go
@@ -21,6 +21,9 @@ func prefixFromQuery(query url.Values) Prefix {
 	}
 	_, prefix.HasPrefix = query["prefix"]
 	_, prefix.HasDelimiter = query["delimiter"]
+	if prefix.Delimiter == "" {
+		prefix.HasDelimiter = false
+	}
 	return prefix
 }
 
